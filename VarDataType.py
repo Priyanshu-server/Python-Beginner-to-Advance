a1b375770fadf003b22824612c707c12ea0bf181
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-08-30 17:13:49 +0530

    Add files via upload

---
+++ VarDataType.py
@@ -0,0 +1,43 @@
+# Basic knowledge of Data Types in python
+x = 5
+y= 10
+print(x,y)
+
+hello = "7"
+world = "10"
+print(hello+world)
+
+
+name = "xDoramming "
+platform = "youtube"
+print(name+platform)
+
+#Printing the type of a variable
+a = 10
+print(type(a))
+
+
+#Type Casting
+
+#str() for string
+#int() for integer
+#float() for float
+#complex() for complex
+
+a = 6
+b = 7
+print(a+b)
+
+a = str(a) # "6"
+b = str(b) #"7"
+print(a+b)
+# you can use \n to break a line
+print("hello \n world")
+a = 10
+a = complex(a)
+print( , a)
+a= 10
+a= float(a)
+print(" Float of a is ", a)
+
+print(type(2+9.0))
