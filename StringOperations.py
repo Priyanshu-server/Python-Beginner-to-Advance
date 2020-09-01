ac5150f5f0b84d0d719379c390fb11883b99615f
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-09-01 15:52:15 +0530

    Add files via upload

---
+++ StringOperations.py
@@ -0,0 +1,16 @@
+#slicing
+#some Functions
+# string.isupper()
+# string.islower()
+# string.upper()
+# string.lower()
+# string.capitalize()
+# string2.isalpha()
+# string2.isalnum()
+# string.count("d")
+# string.endswith("kivymd")
+# string.find("d")
+
+string = "Develop android apps using kivymd"
+string2 = "wqeipfqof"
+print(string.count("d"))
