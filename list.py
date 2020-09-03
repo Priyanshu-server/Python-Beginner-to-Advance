f2f9c0af850ac0b1675738a5c0dc5310c0c00b53
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-09-03 23:05:50 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+# lst = list((1,2,3))
+lst = [500,400,200,100,1200,"xdoramming"]
+lst2 = ['banana','apple']
+lst3 = [400,300,122]
+print(lst)
+# lst.append('youtube')
+# lst.pop(1)
+# lst.clear()
+# b = lst.copy()
+# lst.count(300)
+# lst.extend(lst2)
+# lst.index(500)
+# lst.insert(3,'true')
+# lst.remove(200)
+# lst.reverse()
+lst2.sort()
+print(lst2)
