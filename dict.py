b747aa93fa97497c9ddc66b58db2148c75b401df
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-09-05 19:13:30 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,22 @@
+name = 'mohan'
+a = {20:10,name:10,'shyam':8}
+b = {"emailOfUser@email.com":{"username":"user01","password":"passssword1234"},
+        "emailofUser2@email.com":{"username":"user02","password":"passwordabcd"}}
+# print(b['emailOfUser@email.com']['password'])
+# print(len(a))
+# print(len(b['emailOfUser@email.com']))
+# print(a.get("shyam"))
+# a['shyam'] = 10
+# a['rahul'] = 7
+# a.pop(name)
+# a.popitem()
+# del a['shyam'] (delete the key from the given dict)
+# del a            (to delete the dict completely)
+# c = a.copy()
+# myDict = dict([("shyam",10),("rohan",20)])
+# myDict = dict(brand='xdoramming',country = 'india')
+# print(a.keys())
+# list1 = ["shyam","mohan","rohit"]
+# list2 = 0
+# listDict = {}
+# print(listDict.fromkeys(list1,list2))
