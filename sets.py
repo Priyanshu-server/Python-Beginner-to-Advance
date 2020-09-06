b6d11c33b47d34cdbc96971751079a8cf5b8b143
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-09-06 23:17:03 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,15 @@
+mySet = {"mango","banana","Grrapes","mango"}
+# mySet2 = set(["mango","banana","grapes"])
+# print(mySet)
+# mySet.add("apple")
+# mySet.pop()
+# mySet.remove("mango")
+# mySet2 = mySet.union({"mango","apple"})
+# mySet2 = mySet.intersection({"mango","apple"})
+# mySet2 = mySet.copy()
+# print({1,2,3,4}.issubset({1,2,3,4}))
+#subsets of  {1,2,3,4}
+# {1},{1,2},{2},{3},{4},{2,3}............{1,2,3,4}
+
+a = []
+print(type(a))
