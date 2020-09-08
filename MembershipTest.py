4e73388664abd1bf7b564f1e6e0da8747ee7e255
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-09-08 17:22:52 +0530

    Add files via upload

---
+++ MembershipTest.py
@@ -0,0 +1,30 @@
+# in / not in
+#is / not is
+# user_input = input("Enter sonething:")
+# print(user_input.islower())
+'''
+shop_name = "xDoramming Fruit Shop :: \n"
+print(shop_name)
+shop_items = ["mango","grapes","banana"]
+customer_input = input("Enter your fruit:: ")
+customer_input = customer_input.lower()
+print(customer_input)
+#TO check if all the aplhas is in lower or upper case you can use .isupper() and .islower()
+if customer_input not in shop_items:
+    print("Sorry, Item Unava..")
+else:
+    print("Your order is approved")
+    '''
+# marks = 20
+# user_input = int(input("Enter your marks :: "))
+
+# if type(user_input) is int:
+#     print("Value is correct")
+# else:
+#     print("value is incorrect")
+
+string = "xdoramming"
+if "a" in string:
+    print("a is in the string")
+else:
+    print('a is not in the string')
