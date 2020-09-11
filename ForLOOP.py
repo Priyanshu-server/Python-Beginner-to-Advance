4450e1871a66f34ffe0764c15564d0ad8376a008
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-09-12 00:59:57 +0530

    Add files via upload

---
+++ ForLOOP.py
@@ -0,0 +1,19 @@
+# for i in range(3):
+#     print("hello world")
+
+# lst = ['xdoramming','programming','helloWorld']
+# for i in lst: #1st iteration
+#     print(i)
+
+# lst = [["xdoramming","Programming"],["mukesh","Films"]]
+# for i,j in lst:
+#     print(j)
+
+# dictionary  = {'name':'priyanshu',
+#                 'channelName':'xdoramming'}
+# for key,value in dictionary.items():
+#     print(f"Key is {key} and value is {value}")
+
+lst = ['xdoramming','programming','helloWorld']
+for i in range(len(lst)):
+    print(lst[i])
