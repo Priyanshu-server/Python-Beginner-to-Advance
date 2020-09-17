339d8c2c6be26018cf69cb65c61095a10288c0cc
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-09-17 18:59:07 +0530

    Add files via upload

---
+++ OperatorsInPython.py
@@ -0,0 +1,48 @@
+#Arthmatic
+#+  - * / % **(Power) //
+
+# print(3+4,4-3,2*2,3//2,3**2,sep = "\n") # for integer output you can use round(value)
+#for power you can also use pow(num,power)
+
+#assignment 
+#+= -= *= /= %=
+# i = 0
+# i += 1 # this statement is equals to i = i+1
+# print(f"Value of i is {i}")
+
+#Comparison
+# == != > < >= <=
+# print(2!=2)
+
+#Logical
+#and both statement should be true
+#or at least one statement should be true
+#not
+# time = 'morning'
+# day = 'Tuesday'
+# if (time=='morning')or (day == 'wed'):
+#     print("Tuesday morning")
+
+#Identity
+
+#is 
+#is not
+
+# i = 0
+# if i == 0:
+#     print("i is zero")
+
+
+#Membership
+#in 
+#not in
+
+# a = "xDoramming"
+# if "D" in a:
+#     print("d exists in a")
+# lst = [1,3,4,"hello"]
+# if 2 not in lst:
+#     print("2 does not exist")
+# di = {'one':1}
+# if 'one' in di:
+#     print("one exists")
