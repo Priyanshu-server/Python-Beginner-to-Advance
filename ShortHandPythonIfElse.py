9919750f90cd35c642e652689edd31a19fe17b71
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-09-19 00:01:38 +0530

    Add files via upload

---
+++ ShortHandPythonIfElse.py
@@ -0,0 +1,11 @@
+#Short Hand If Else
+num1 = int(input("Enter Value for num1 :: "))
+num2 = int(input("Enter Value for num2 :: "))
+
+# if num1>num2:
+#     print("num1 is greater than num2")
+# else:
+#     print("num2 is greater than num1")
+
+
+print("Num1 is greater than num2") if (num1>num2) else print("Num2 is greater than num1")
