99a52200b35e48b2204c427347a249e1227a03e2
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-09-23 00:22:41 +0530

    Add files via upload

---
+++ FunctionsPart1.py
@@ -0,0 +1,29 @@
+# def printStatement():
+#     print("hello world")
+#     print("hello developer !")
+
+
+# def printStatement(a,b):
+#     print(f"Value of a is {a} and value of b is {b}")
+
+# printStatement(5,10)
+
+
+# def add(a,g):
+#     print(a+g)
+
+
+# num1 = int(input("Enter first digit : "))
+# num2 = int(input("Enter second digit : "))
+
+# add(num1,num2)
+
+
+def add():
+    print(num1+num2)
+
+
+num1 = int(input("Enter first digit : "))
+num2 = int(input("Enter second digit : "))
+
+add()
