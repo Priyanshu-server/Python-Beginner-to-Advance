4173ce85a50148e046fbb6e37d2c86426c75ab17
Priyanshu-server <69322912+Priyanshu-server@users.noreply.github.com>
2020-10-08 00:15:03 +0530

    Add files via upload

---
+++ lambdaFunc.py
@@ -0,0 +1,13 @@
+#lambda function
+
+# def sum(a,b):
+#     return a+b
+
+# print(sum(2,5))
+
+# (lambda a,b: a+b)(2,5) 
+
+# add = lambda a,b : a+b if a<b else a-b
+add = lambda a,b : a+b
+print(add(2,3))
+add(3,2))
